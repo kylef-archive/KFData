4a647393b2e347e5c96c7d0979194e3e27e28762
Kyle Fuller <inbox@kylefuller.co.uk>
2012-12-12 01:12:42 +0000

    Create KFDataTableViewController, a standard UIViewController for tables

---
+++ Classes/iOS/KFDataTableViewController.h
@@ -0,0 +1,27 @@
+//
+//  KFDataTableViewController.h
+//  KFData
+//
+//  Created by Kyle Fuller on 08/11/2012.
+//  Copyright (c) 2012 Kyle Fuller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "KFFetchedResultsTableController.h"
+
+@class NSManagedObjectContext;
+@class KFDataStore;
+
+@interface KFDataTableViewController : UITableViewController <KFFetchedResultsTableControllerDelegate>
+
+@property (nonatomic, strong) KFFetchedResultsTableController *fetchedResultsTableController;
+@property (nonatomic, strong, readonly) NSManagedObjectContext *managedObjectContext;
+
+- (id)initWithDataStore:(KFDataStore*)dataStore;
+- (id)initWithManagedObjectContext:(NSManagedObjectContext*)managedObjectContext;
+
+- (void)setFetchRequest:(NSFetchRequest*)fetchRequest
+     sectionNameKeyPath:(NSString*)sectionNameKeyPath;
+
+@end
+
---
+++ Classes/iOS/KFDataTableViewController.m
@@ -0,0 +1,86 @@
+//
+//  KFDataTableViewController.m
+//  KFData
+//
+//  Created by Kyle Fuller on 08/11/2012.
+//  Copyright (c) 2012 Kyle Fuller. All rights reserved.
+//
+
+#import "KFDataStore.h"
+#import "KFFetchedResultsTableController.h"
+#import "KFDataTableViewController.h"
+
+@implementation KFDataTableViewController
+
+#pragma mark -
+
+- (id)initWithDataStore:(KFDataStore*)dataStore {
+    NSManagedObjectContext *managedObjectContext = [dataStore managedObjectContextWithConcurrencyType:NSMainQueueConcurrencyType];
+
+    if (self = [self initWithManagedObjectContext:managedObjectContext]) {
+
+    }
+
+    return self;
+}
+
+- (id)initWithManagedObjectContext:(NSManagedObjectContext*)managedObjectContext
+{
+    if (self = [self init]) {
+        _managedObjectContext = managedObjectContext;
+    }
+
+    return self;
+}
+
+#pragma mark -
+
+/*
+- (void)loadView {
+    UITableView *tableView = [[UITableView alloc] init];
+    [tableView setDelegate:self];
+    [self setTableView:tableView];
+    [self setView:tableView];
+}
+*/
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    NSManagedObjectContext *managedObjectContext = [self managedObjectContext];
+    KFFetchedResultsTableController *fetchedResultsTableController = [[KFFetchedResultsTableController alloc] initWithManagedObjectContext:managedObjectContext
+                                                                                                                                 tableView:[self tableView]
+                                                                                                                                  delegate:self];
+
+    [self setFetchedResultsTableController:fetchedResultsTableController];
+}
+
+#pragma mark -
+
+- (void)setFetchRequest:(NSFetchRequest*)fetchRequest
+     sectionNameKeyPath:(NSString*)sectionNameKeyPath
+{
+    [[self fetchedResultsTableController] setFetchRequest:fetchRequest
+                                       sectionNameKeyPath:sectionNameKeyPath];
+}
+
+#pragma mark -
+
+- (NSString*)fetchedResultsTableController:(KFFetchedResultsTableController *)fetchedResultsTableController
+           reuseIdentifierForManagedObject:(NSManagedObject *)managedObject
+                               atIndexPath:(NSIndexPath *)indexPath
+{
+    NSAssert(NO, @"Subclasses need to overwrite this method");
+    return nil;
+}
+
+- (void)fetchedResultsTableController:(KFFetchedResultsTableController *)fetchedResultsTableController
+                       configuredCell:(UITableViewCell *)cell
+                     forManagedObject:(NSManagedObject *)managedObject
+                          atIndexPath:(NSIndexPath *)indexPath
+{
+    NSAssert(NO, @"Subclasses need to overwrite this method");
+}
+
+@end
+
