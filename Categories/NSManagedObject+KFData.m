8bb7c2687e9dce42d9a2bdfa46c10c72c2ed817d
Kyle Fuller <inbox@kylefuller.co.uk>
2013-02-18 17:07:01 +0000

    "NSInteger should not be used in format strings"

--- Categories/NSManagedObject+KFData.m
+++ Categories/NSManagedObject+KFData.m
@@ -18,7 +18,7 @@
 	NSArray* results = [managedObjectContext executeFetchRequest:fetchRequest error:&error];
 	
 	if (error) {
-		NSLog(@"executeFetchRequest failed with %d %@", [error code], [error description]);
+		NSLog(@"executeFetchRequest failed with %ld %@", (long)[error code], [error description]);
 	}
     
     return results;
