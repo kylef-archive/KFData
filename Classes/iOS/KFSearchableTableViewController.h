b0cd88ae9efd252b1f62392ad38638b7734c56cd
Calvin Cestari <calvin@calvincestari.com>
2013-04-15 09:48:35 -0700

    Updated for appledoc

--- Classes/iOS/KFSearchableTableViewController.h
+++ Classes/iOS/KFSearchableTableViewController.h
@@ -8,11 +8,28 @@
 
 #import "KFDataTableViewController.h"
 
+/**
+ KFSearchableTableViewController is a subclass of KFDataTableViewController which adds
+ a UISearchBar to the tableview as a header.
+ */
+
 @interface KFSearchableTableViewController : KFDataTableViewController <UISearchBarDelegate, NSFetchedResultsControllerDelegate>
 
 @property (nonatomic, strong, readonly) UISearchBar* searchBar;
 @property (nonatomic, assign, readonly, getter=isFiltering) BOOL filtering;
 
 - (id)initWithManagedObjectContext:(NSManagedObjectContext*)managedObjectContext;
+
+/** @name Subclass override */
+
+/** This method will return the predicate to be used by the NSFetchedResultsController. It is
+ important to note that the predicate returned will be used to form a compound predicate in
+ conjunction with the original predicate supplied to the NSFetchedResultsController. When the
+ search is cancelled or searchText is empty the original predicate alone will be used.
+ 
+ @param searchText The text contents of the UISearchBar.
+ @return Predicate to be used for data lookup.
+ */
+- (NSPredicate*)predicateForSearchText:(NSString*)searchText;
 
 @end
