b959383e8c11cae2f25709c02dda1ceb3cc18217
Kyle Fuller <inbox@kylefuller.co.uk>
2013-08-19 00:03:58 +0100

    KFDataViewController doesn't need to listen for saves

--- KFData/UI/KFDataViewController.m
+++ KFData/UI/KFDataViewController.m
@@ -19,31 +19,9 @@
 - (instancetype)initWithManagedObjectContext:(NSManagedObjectContext*)managedObjectContext {
     if (self = [self init]) {
         _managedObjectContext = managedObjectContext;
-
-        NSManagedObjectContext *parentContext = [managedObjectContext parentContext];
-        if (parentContext) {
-            [[NSNotificationCenter defaultCenter] addObserver:self
-                                                     selector:@selector(mergeChanges:)
-                                                         name:NSManagedObjectContextDidSaveNotification
-                                                       object:parentContext];
-        }
     }
 
     return self;
-}
-
-- (void)dealloc {
-    [[NSNotificationCenter defaultCenter] removeObserver:self];
-}
-
-#pragma mark - Notifications
-
-- (void)mergeChanges:(NSNotification*)notification {
-    NSManagedObjectContext *managedObjectContext = [self managedObjectContext];
-
-    [managedObjectContext performBlock:^{
-        [managedObjectContext mergeChangesFromContextDidSaveNotification:notification];
-    }];
 }
 
 @end
